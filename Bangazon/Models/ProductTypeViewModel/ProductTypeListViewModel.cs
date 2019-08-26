b43d57117cd68f1c6693619701f9f5e9f738cf2a
deliotclarke <deliotclarke@gmail.com>
2019-08-26 14:57:06 -0500

    created product type view model folder and product type list view model

---
+++ Bangazon/Models/ProductTypeViewModel/ProductTypeListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models.ProductTypeViewModel
+{
+    public class ProductTypeDetailViewModel
+    {
+        public int ProductTypeId { get; set; }
+        public string Label { get; set; }
+        public int ProductCount { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
