a3aaa24c9312f1f8ce39927b5d5e2d6f54169d71
deliotclarke <deliotclarke@gmail.com>
2019-08-27 13:39:34 -0500

    i'm close... maybe. checking out for new pr.

--- Bangazon/Areas/Identity/Pages/Account/Manage/PaymentType.cshtml.cs
+++ Bangazon/Areas/Identity/Pages/Account/Manage/PaymentType.cshtml.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Bangazon.Models;
@@ -14,8 +16,60 @@ namespace Bangazon.Areas.Identity.Pages.Account.Manage
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public void OnGet()
+        public PaymentTypeModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [DataType(DataType.Date)]
+            [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+            public DateTime DateCreated { get; set; }
+
+            [Required]
+            [StringLength(55)]
+            public string Description { get; set; }
+
+            [Required]
+            [StringLength(20)]
+            public string AccountNumber { get; set; }
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            await _signInManager.RefreshSignInAsync(user);
+            StatusMessage = "Your profile has been updated";
+            return RedirectToPage();
         }
     }
 }
